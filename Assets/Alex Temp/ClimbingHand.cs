6410b9767459d9687505f9984e8a1305bcf1e741
Alexander Freyr <alexander-freyr.torgeirsson.3043@student.uu.se>
2024-10-19 18:20:24 +0200

    hand rotation

--- Assets/Alex Temp/ClimbingHand.cs
+++ Assets/Alex Temp/ClimbingHand.cs
@@ -5,6 +5,13 @@ public class ClimbingHand : MonoBehaviour
 {
     public bool arrow;
     public float climbSpeed = 5;
+    public float rotationForce = 100;
+
+    HingeJoint2D hinge;
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint2D>();
+    }
 
     void FixedUpdate()
     {
@@ -24,6 +31,35 @@ public class ClimbingHand : MonoBehaviour
                 climb -= 1;
         }
 
-        GetComponent<HingeJoint2D>().connectedAnchor += climb * climbSpeed * Time.deltaTime * Vector2.right;
+        hinge.connectedAnchor += climb * climbSpeed * Time.deltaTime * Vector2.right;
+
+        float rotate = 0;
+        if (Input.GetKey(KeyCode.Q))
+            rotate -= 1;
+        if (Input.GetKey(KeyCode.E))
+            rotate += 1;
+
+        // add torque at hinge connection pivot
+        ApplyTorqueAtPivot(hinge.connectedBody, hinge.connectedAnchor, rotate * rotationForce);
+        
+    }
+
+    static void ApplyTorqueAtPivot(Rigidbody2D rb, Vector2 pivotPoint, float forceMagnitude)
+    {
+        Vector2 worldPivotPoint = rb.transform.TransformPoint(pivotPoint);
+
+        Vector2 offset = (Vector2)rb.transform.right * .5f;
+        Vector2 pointA = worldPivotPoint + offset;
+        Vector2 pointB = worldPivotPoint - offset;
+
+        Vector2 torqueAxis = rb.transform.up;
+
+        rb.AddForceAtPosition(-torqueAxis * forceMagnitude, pointA);
+        rb.AddForceAtPosition(torqueAxis * forceMagnitude, pointB);
+
+        Debug.DrawRay(pointB, torqueAxis);
+        Debug.DrawRay(pointA, -torqueAxis);
+
+        Debug.DrawLine(pointA, pointB, Color.red);
     }
 }
